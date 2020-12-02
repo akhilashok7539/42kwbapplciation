ef3ffa7bda4377103108f083a9843c0d3a94cee8
akhilashok7539 <akhilashok7539@gmail.com>
2020-12-02 02:12:39 -0800

    upations

--- src/app/testcomponent/testcomponent.component.ts
+++ src/app/testcomponent/testcomponent.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-testcomponent',
@@ -6,10 +7,49 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./testcomponent.component.css']
 })
 export class TestcomponentComponent implements OnInit {
-
-  constructor() { }
+  // disabled=false;
+  // disable=false;
+  // regifrom:FormGroup;
+  // nam2;
+  // nam1;
+  constructor(private fb:FormBuilder) { }
 
   ngOnInit(): void {
-  }
+    // this.regifrom = this.fb.group({
+    //   nam1: new FormControl({value: '', disabled: false},Validators.required),
 
+    //   nam2: new FormControl({value: '', disabled: false},Validators.required),
+    // })
+  }
+  // onKeydown(a){
+    
+  //   this.disabled=true;
+
+  //   if(a =='')
+  //   {   
+  //     console.log("disabled field percetange");
+    
+  //   this.regifrom.get('nam2').enable();
+
+  //   }
+  //   else{
+  //   this.disabled=true;
+  //   this.regifrom.get('nam2').disable();
+
+  //   }
+  // }
+  // onKeydown1(a)
+  // {
+  //   this.disable = true;
+  //   if(a =='')
+  //   {
+  //     console.log("disabled field amout");
+  //     this.disable=false;
+      
+  //   }
+  //   else{
+  //   this.disable=true;
+
+  //   }
+  // }
 }
